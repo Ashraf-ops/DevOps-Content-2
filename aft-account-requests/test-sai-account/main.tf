5a2a1c1bd5506703e5a96b1e2b8027c8633caa77
asshu-devops <ashrafshaik02@gmail.com>
2025-05-22 13:01:59 +0530

    Update main.tf

--- aft-account-requests/test-sai-account/main.tf
+++ aft-account-requests/test-sai-account/main.tf
@@ -1,27 +1,19 @@
 terraform {
   required_version = ">= 1.3.0"
+}
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0"
-    }
+resource "awscc_aft_account_request" "new_account" {
+  account_name                = "test-sai"
+  account_email               = "test-sai1234@yop.mail"
+  sso_user_email              = "test-sai12345@yop.mail"
+  sso_user_first_name         = "testing"
+  sso_user_last_name          = "sai"
+  managed_organizational_unit = "sandbox"
+  account_customizations_name = "admin"
+  control_tower_parameters = {
+    "SSOUserEmail"              = "test-sai12345@yop.mail"
+    "SSOUserFirstName"          = "testing"
+    "SSOUserLastName"           = "sai"
+    "ManagedOrganizationalUnit" = "sandbox"
   }
-}
-
-provider "aws" {
-  region = "us-east-1" # Use your AFT home region
-}
-
-module "aft_account_request" {
- source = "git::https://github.com/aws-ia/terraform-aws-control_tower_account_factory.git?ref=main"
-
-  account_name                = var.account_name
-  account_email               = var.account_email
-  sso_email                   = var.sso_email
-  sso_first_name              = var.sso_first_name
-  sso_last_name               = var.sso_last_name
-  managed_ou                  = var.managed_ou
-  account_customizations_name = var.account_customizations_name
-  tags                        = var.tags
 }
