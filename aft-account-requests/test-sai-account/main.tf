a0ac0a4208cec324f3efef6d28849df9d0768392
asshu-devops <ashrafshaik02@gmail.com>
2025-05-22 13:56:32 +0530

    Update main.tf

--- aft-account-requests/test-sai-account/main.tf
+++ aft-account-requests/test-sai-account/main.tf
@@ -2,11 +2,14 @@ module "aft_account_request" {
   source  = "aws-ia/control_tower_account_factory/aws"
   version = "~> 1.8.0"
 
-  account_email               = "test-sai1234@yop.mail"
-  account_name                = "test-sai"
-  sso_user_email              = "test-sai12345@yop.mail"
-  sso_user_first_name         = "testing"
-  sso_user_last_name          = "sai"
-  managed_organizational_unit = "sandbox"
-  account_customizations_name = "admin"
+  tf_backend_secondary_region = var.tf_backend_secondary_region
+  log_archive_account_id      = var.log_archive_account_id
+  audit_account_id            = var.audit_account_id
+  aft_management_account_id   = var.aft_management_account_id
+  ct_management_account_id    = var.ct_management_account_id
+  ct_home_region              = var.ct_home_region
+
+  control_tower_parameters    = var.control_tower_parameters
+  account_customizations_name = var.account_customizations_name
+  tags                        = var.tags
 }
