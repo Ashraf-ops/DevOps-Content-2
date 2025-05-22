3d4e38ccae1bf1a3a3625d37f3e506d5ca264cc3
asshu-devops <ashrafshaik02@gmail.com>
2025-05-22 13:05:57 +0530

    Update main.tf

--- aft-account-requests/test-sai-account/main.tf
+++ aft-account-requests/test-sai-account/main.tf
@@ -1,19 +1,12 @@
-terraform {
-  required_version = ">= 1.3.0"
-}
+module "aft_account_request" {
+  source  = "aws-ia/control_tower_account_factory/aws"
+  version = "~> 1.8.0"
 
-resource "awscc_aft_account_request" "new_account" {
-  account_name                = "test-sai"
   account_email               = "test-sai1234@yop.mail"
+  account_name                = "test-sai"
   sso_user_email              = "test-sai12345@yop.mail"
   sso_user_first_name         = "testing"
   sso_user_last_name          = "sai"
   managed_organizational_unit = "sandbox"
   account_customizations_name = "admin"
-  control_tower_parameters = {
-    "SSOUserEmail"              = "test-sai12345@yop.mail"
-    "SSOUserFirstName"          = "testing"
-    "SSOUserLastName"           = "sai"
-    "ManagedOrganizationalUnit" = "sandbox"
-  }
 }
